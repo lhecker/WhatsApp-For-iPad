50c7b92fd31830968e66b366c29629054c384782
Leonard Hecker <leonard@hecker.io>
2021-01-31 16:30:06 +0100

    Personal adjustments for this fork

--- WhatsAppiPad/ViewController.swift
+++ WhatsAppiPad/ViewController.swift
@@ -10,22 +10,13 @@ import WebKit
 import UserNotifications
 import CoreLocation
 
-
-private var thisViewController: ViewController? 
-
-
 class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKNavigationDelegate, CLLocationManagerDelegate, UIScrollViewDelegate {
-    
     var locationManager: CLLocationManager?
-    
-    static var instance: ViewController {
-        guard let thisViewController = thisViewController else { fatalError() } 
-        return thisViewController
-    }
-    
-    
+    var webView: WKWebView!
+    var backButton: UIButton!
+
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        
+
         if (message.name == "openDocument") {
             //future usage
         }
@@ -36,21 +27,21 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKN
         {
             var messageSender = "";
             var messageBody = "";
-            
-            var messageData = message.body as! String
+
+            let messageData = message.body as! String
             if let index = messageData.firstIndex(of: "|") {
                 messageSender = String(messageData[..<index])
                 let indexNext = messageData.index(after: index)
                 messageBody = String(messageData[indexNext...])
             }
-            
+
             let content = UNMutableNotificationContent()
             content.title = messageSender//"WhatsApp Message"
             content.body = messageBody//message.body as! String
-            
+
             let uuidString = UUID().uuidString
             let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: nil)
-            
+
             let notificationCenter = UNUserNotificationCenter.current()
             notificationCenter.add(request) { (error) in
                 if error != nil {
@@ -59,31 +50,22 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKN
             }
         }
     }
-    
-    
-    func webView(_ webView: WKWebView,
-                 decidePolicyFor navigationAction: WKNavigationAction,
-                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        let url = navigationAction.request.url
-        if ((url?.absoluteString.hasPrefix("https://web.whatsapp.com")) != false)
-        {
-            decisionHandler(.allow)
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let url = navigationAction.request.url else {
+            return
         }
-        else if ((url?.absoluteString.hasPrefix("blob:")) != false)
-        {
+        
+        if (url.absoluteString.hasPrefix("https://web.whatsapp.com")) {
             decisionHandler(.allow)
-        }
-        else
-        {
+        } else if (url.absoluteString.hasPrefix("blob:")) {
+            decisionHandler(.allow)
+        } else {
             decisionHandler(.cancel)
-            UIApplication.shared.openURL(url!)
+            UIApplication.shared.open(url)
         }
-	}
-    
-    
-    var webView: WKWebView!
-    var backButton: UIButton!
-    
+    }
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
             // you're good to go!
@@ -91,32 +73,28 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKN
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last {
+        if locations.last != nil {
             reloadData()
         }
     }
-    
-    func reloadData()
-    {
+
+    func reloadData() {
         webView.evaluateJavaScript("1+1", completionHandler: nil)
     }
-    
-   
+
     func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
        scrollView.pinchGestureRecognizer?.isEnabled = false
     }
-    
+
     override func loadView() {
-        
         let userScriptURL = Bundle.main.url(forResource: "UserScript", withExtension: "js")!
         let userScriptCode = try! String(contentsOf: userScriptURL)
         let userScript = WKUserScript(source: userScriptCode, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-        
         let webConfiguration = WKWebViewConfiguration()
-        
+
         webConfiguration.userContentController.addUserScript(userScript)
         webConfiguration.userContentController.add(self, name: "notify")
-        
+
         //For Content Blocker
         let blockRules = """
            [
@@ -131,28 +109,25 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKN
                }
            ]
         """
-        
-        WKContentRuleListStore.default().compileContentRuleList(
-            forIdentifier: "ContentBlockingRules",
-            encodedContentRuleList: blockRules) { (contentRuleList, error) in
 
-                if let error = error {
-                    return
-                }
-                webConfiguration.userContentController.add(contentRuleList!)
+        WKContentRuleListStore.default().compileContentRuleList(forIdentifier: "ContentBlockingRules", encodedContentRuleList: blockRules) { (contentRuleList, error) in
+            if error != nil {
+                return
+            }
+            webConfiguration.userContentController.add(contentRuleList!)
         }
-        
+
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
         webView.navigationDelegate = self
         webView.scrollView.delegate = self
         view = webView
     }
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         locationManager = CLLocationManager()
         locationManager?.allowsBackgroundLocationUpdates = true
         locationManager?.pausesLocationUpdatesAutomatically = false
@@ -162,16 +137,14 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler,WKN
         locationManager?.startUpdatingLocation()
         locationManager?.startMonitoringVisits()
         locationManager?.startMonitoringSignificantLocationChanges()
-        
+
         let myURL = URL(string:"https://web.whatsapp.com")
-        var myRequest = URLRequest(url: myURL!)
-        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
-        
+        let myRequest = URLRequest(url: myURL!)
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"
+
         webView.scrollView.alwaysBounceVertical = false
         webView.scrollView.alwaysBounceHorizontal = false
         webView.scrollView.bounces = false
         webView.allowsBackForwardNavigationGestures = true
         webView.load(myRequest)
     }}
-
-
